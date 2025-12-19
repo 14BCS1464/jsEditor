fe43bdb42ebdcb5dfcf90e941517e26bfc45e507
14BCS1464 <prajapatisunil135@gmail.com>
2025-12-19 20:42:29 +0530

    feat: implement real-time collaborative editing with Socket.IO, Monaco Editor, and local storage persistence.

--- src/editor/script.js
+++ src/editor/script.js
@@ -383,7 +383,7 @@ require(["vs/editor/editor.main"], async function () {
     //     container.classList.add('column-layout');
     //     rowLayoutBtn.classList.remove('active');
     //     columnLayoutBtn.classList.add('active');
-    // }
+// }
 
     // rowLayoutBtn.addEventListener('click', function () {
     //     container.classList.remove('column-layout');
@@ -1242,7 +1242,7 @@ require(["vs/editor/editor.main"], async function () {
         controlsDiv.appendChild(copyBtn);
     }
 
-    // addCopyRoomButton();
+    addCopyRoomButton();
 
     monaco.languages.registerCompletionItemProvider("javascript", {
         provideCompletionItems: () => {
